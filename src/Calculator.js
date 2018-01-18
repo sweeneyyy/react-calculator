112954e25e283adb0b58c8be13aaeab717372a95
sweeneyyy <sweeneyarnett@gmail.com>
2018-01-17 19:38:20 -0800

    calculator adds yay

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,37 +7,48 @@ class Calculator extends Component {
       total: 0,
       val1: 0,
       val2: 0
-    }
+    };
 
     this.calculateTotal = this.calculateTotal.bind(this);
-    this.updateValues = this.updateValues.bind(this);
+    this.updateValueOne = this.updateValueOne.bind(this);
+    this.updateValueTwo = this.updateValueTwo.bind(this);
 
   }
 
   calculateTotal(event){
-    this.setState({total: this.state.val1 + this.state.val2});
+    event.preventDefault();
+    let num1 = this.state.val1;
+    let num2 = this.state.val2;
+    this.setState({total: parseInt(num1) + parseInt(num2)});
+    console.log('added');
+  };
+
+  updateValueOne(event){
+    this.setState({val1: event.target.value});
   }
 
-  updateValues(event){
-    this.setState({val1: event.target.val1, val2: event.target.val2});
+  updateValueTwo(event){
+    this.setState({val2: event.target.value});
   }
 
-
+  
   render() {
     return (
-      <div className="Calculator">
+      // <div className="Calculator">
         <div className="container">
           <h1>Add with React!</h1>
             <form onSubmit={this.calculateTotal}>
-              <input type="text" value={this.state.val1} onChange={this.updateValues}/>
-              <span>+</span>
-              <input type="text" value={this.state.val2} onChange={this.updateValues} />
-              <span>=</span>
-              <h3>{this.state.total}</h3>
+              <div className="add">
+                <input type="integer" value={this.state.val1} onChange={this.updateValueOne} />
+                <span>+</span>
+                <input type="integer" value={this.state.val2} onChange={this.updateValueTwo} />
+                <span>=</span>
+                <h3>{this.state.total}</h3>
+                <input type="submit" value="submit" />
+              </div>
             </form>
-            <input type="submit" value="calculate" />
           </div>
-        </div>
+        // </div>
       );
   }
 }
